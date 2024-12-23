25dced7a6718c351371e005501808322e3835231
Dev Ashrit Behera <devashritcode@gmail.com>
2024-12-23 16:05:17 +0530

    initial commit

---
+++ src/test/java/com/learnspring/playerapi/PlayerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.playerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
